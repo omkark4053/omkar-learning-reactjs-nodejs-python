5d238f2d4ea1aee50f598b92332b3045d9fa6f9c
omkark4053 <omkarkadam4053@gmail.com>
2025-03-15 10:33:55 +0530

    added file

---
+++ opercond.js
@@ -0,0 +1,99 @@
+/*operators and conditionals
+statements.*/
+
+
+//Arithmetic operators
+let a = 5
+let b = 2
+
+console.log(" a = ",a ,  " & b = ",b )
+//console.log("a + b = ", a + b)
+//console.log("a - b = ", a - b)
+//console.log("a * b = ", a * b)
+//console.log("a / b = ", a / b)
+
+
+
+// Modulus
+//console.log("a % b = ", a % b)
+
+/*Exponentiation (maths mai restu power wala
+use ** for this */
+//console.log("a ** b = ", a ** b)
+
+/* Unary Operarors 
+1. increment {++}
+2. decrement {--}*/
+
+//let a = 5
+//let b = 2
+
+console.log(" a = ",a ,  " & b = ",b ) 
+//a = a + 1;
+a++ 
+console.log(" a = ", a);
+
+//a = a - 2;
+a--
+console.log("a = ", a)
+
+/* 1.a++ (post)
+   2.++a (pre)*/
+   //console.log(" a = ",a ,  " & b = ",b ) 
+   //console.log(" ++a = ", ++a); //6
+
+   //console.log(" a++ = ", a++); //6
+   //console.log(" a " , a) //7
+
+   //console.log(" --a = ", --a); //6
+
+   //console.log(" a-- = ", a--); //6
+   //console.log(" a = ", a--)//5
+
+// Asignment operators
+  
+ //let a = 5
+ //let b = 2
+
+ a += 4 //9
+ console.log(" a = ", a )
+
+ a -= 4 //1
+ console.log(" a = ", a )
+
+ a *= 4 //20
+ console.log(" a = ", a ) 
+
+ a /= 4 //1.25
+ console.log(" a = ", a ) 
+
+ a %= 4 //1
+ console.log(" a = ", a ) 
+
+ a **= 4 //625
+ console.log(" a = ", a ) 
+
+
+
+
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
